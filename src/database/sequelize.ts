b7e6dc34b2d61caa5274dd233ad816beeed365ff
Cezar Fernando <czarfbc@gmail.com>
2023-12-12 19:01:34 -0300

    conexão com o banco concluida

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import { Sequelize } from 'sequelize';
 const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(`${DATABASE_URL}`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false,
   native: false,
 });

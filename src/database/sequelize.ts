d96bac487af59755139ae85fa03ad4b60d65991a
Cezar Fernando <czarfbc@gmail.com>
2023-12-14 13:12:15 -0300

    auth concluido

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -2,6 +2,10 @@ require('dotenv').config();
 import { Sequelize } from 'sequelize';
 const { DATABASE_URL } = process.env;
 
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in .env file');
+}
+
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false,
   native: false,

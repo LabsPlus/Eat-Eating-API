77ec284216e2a5cc1f83d4bc391d65b637612db1
bruno34154 <www.brunomesenga@gmail.com>
2024-01-15 08:09:19 -0300

    Back-86az0w6dn-Block ip later 3 atempts:feat

--- src/__tests__/emailService.test.ts
+++ src/__tests__/emailService.test.ts
@@ -20,7 +20,12 @@ describe('Email', () => {
       close: jest.fn(),
     } as any);
 
-    const result = await email.sendEmail('example@example.com', 'Subject', 'Content');
+    const result = await email.sendEmail({
+      destination: 'example@example.com',
+      subject: 'Subject',
+      content: 'Content',
+      link: 'https://example.com',
+    });
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
       host: process.env.HOST,
@@ -41,7 +46,12 @@ describe('Email', () => {
       close: jest.fn(),
     } as any);
 
-    await expect(email.sendEmail('example@example.com', 'Subject', 'Content')).rejects.toThrow('email not sended');
+    await expect(email.sendEmail({
+      destination: 'example@example.com',
+      subject: 'Subject',
+      content: 'Content',
+      link: 'https://example.com',
+    })).rejects.toThrow('email not sended');
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
       host: process.env.HOST,

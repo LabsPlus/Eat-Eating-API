39dd1d5c6706f72aa25e595664aa8847fb54f21a
bruno34154 <www.brunomesenga@gmail.com>
2024-01-17 19:34:01 -0300

    refactor-86az36ehz-refactoring error mensages and interfaces for padronization:refactor

--- src/intefaces/users.interfaces.ts
+++ src/intefaces/users.interfaces.ts
@@ -8,3 +8,14 @@ export interface IUsersAuth {
   email: string;
   password: string;
 }
+
+export interface IUsersUpdatePassword {
+    newPassword: string;
+    token?: string | undefined;
+    email?: string;
+}
+
+export interface IUserForgotPassword {
+    email: string;
+    ip: string | undefined;
+}

2def3f8720280c2a930bec8094e6a84f8529ca04
Cezar Fernando <czarfbc@gmail.com>
2024-01-29 17:44:36 -0300

    error middleware login

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,77 @@
-import express, {Application} from 'express';
-import {LoginRoutes} from './routes/login.routes';
-import cors, {CorsOptions} from 'cors';
-import {CategoryRoutes} from "./routes/category.routes";
-import {UserRoutes} from "./routes/user.routes";
-import {TypeStudentGrantRoutes} from "./routes/type.student.grant.routes";
-
+import 'express-async-errors';
+import express, { Application } from 'express';
+import { LoginRoutes } from './routes/login.routes';
+import cors, { CorsOptions } from 'cors';
+import { CategoryRoutes } from './routes/category.routes';
+import { UserRoutes } from './routes/user.routes';
+import { TypeStudentGrantRoutes } from './routes/type.student.grant.routes';
+import { errorMiddleware } from './middlewares/error.middlewares';
 export class App {
-    private app: Application;
+  private app: Application;
 
-    constructor(corsConfig?: CorsOptions) {
-        this.app = express();
-        this.middleware(corsConfig);
-        this.setupLoginRoutes();
-        this.setupCategoryRoutes();
-        this.setupTypeStudentGrantRoutes();
-        this.setupUserRoutes();
-    }
+  constructor(corsConfig?: CorsOptions) {
+    this.app = express();
+    this.middleware(corsConfig);
+    this.setupLoginRoutes();
+    this.setupCategoryRoutes();
+    this.setupTypeStudentGrantRoutes();
+    this.setupUserRoutes();
+  }
 
-    private middleware(corsConfig?: CorsOptions) {
-        this.app.use(express.json());
-        this.app.use(cors(corsConfig));
-        this.app.use(express.urlencoded({extended: true}));
-    }
+  listen(port: number) {
+    this.app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
+  }
 
-    listen(port: number) {
-        this.app.listen(port, () => {
-            console.log(`Servidor rodando na porta ${port}`);
-        });
-    }
+  private setupCategoryRoutes() {
+    const categoryRoutes = new CategoryRoutes();
+    const categoryBaseRoute = '/category';
 
-    private setupCategoryRoutes() {
-        const categoryRoutes = new CategoryRoutes();
-        const categoryBaseRoute = '/category';
+    this.app.use(categoryBaseRoute, categoryRoutes.postRoutes());
+    this.app.use(categoryBaseRoute, categoryRoutes.putRoutes());
+    this.app.use(categoryBaseRoute, categoryRoutes.getRoutes());
+    this.app.use(categoryBaseRoute, categoryRoutes.deleteRoutes());
+  }
 
-        this.app.use(categoryBaseRoute, categoryRoutes.postRoutes());
-        this.app.use(categoryBaseRoute, categoryRoutes.putRoutes());
-        this.app.use(categoryBaseRoute, categoryRoutes.getRoutes());
-        this.app.use(categoryBaseRoute, categoryRoutes.deleteRoutes());
-    }
+  private setupUserRoutes() {
+    const userRoutes = new UserRoutes();
+    const userBaseRoute = '/user';
 
-    private setupUserRoutes() {
-        const userRoutes = new UserRoutes();
-        const userBaseRoute = '/user';
+    this.app.use(userBaseRoute, userRoutes.postRoutes());
+    this.app.use(userBaseRoute, userRoutes.putRoutes());
+    this.app.use(userBaseRoute, userRoutes.getRoutes());
+    this.app.use(userBaseRoute, userRoutes.deleteRoutes());
+  }
 
-        this.app.use(userBaseRoute, userRoutes.postRoutes());
-        this.app.use(userBaseRoute, userRoutes.putRoutes());
-        this.app.use(userBaseRoute, userRoutes.getRoutes());
-        this.app.use(userBaseRoute, userRoutes.deleteRoutes());
-    }
+  private setupTypeStudentGrantRoutes() {
+    const typeStudentGrantRoutes = new TypeStudentGrantRoutes();
+    const typeStudentGrantBaseRoute = '/typeStudentGrant';
 
-    private setupTypeStudentGrantRoutes() {
-        const typeStudentGrantRoutes = new TypeStudentGrantRoutes();
-        const typeStudentGrantBaseRoute = '/typeStudentGrant';
+    this.app.use(
+      typeStudentGrantBaseRoute,
+      typeStudentGrantRoutes.postRoutes(),
+    );
+    this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.putRoutes());
+    this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.getRoutes());
+    this.app.use(
+      typeStudentGrantBaseRoute,
+      typeStudentGrantRoutes.deleteRoutes(),
+    );
+  }
 
-        this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.postRoutes());
-        this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.putRoutes());
-        this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.getRoutes());
-        this.app.use(typeStudentGrantBaseRoute, typeStudentGrantRoutes.deleteRoutes());
-    }
+  private setupLoginRoutes() {
+    const loginRoutes = new LoginRoutes();
+    const loginBaseRoute = '/login';
 
-    private setupLoginRoutes() {
-        const loginRoutes = new LoginRoutes();
-        const loginBaseRoute = '/login';
+    this.app.use(loginBaseRoute, loginRoutes.patchRoutes());
+    this.app.use(loginBaseRoute, loginRoutes.postRoutes());
+  }
 
-        this.app.use(loginBaseRoute, loginRoutes.patchRoutes());
-        this.app.use(loginBaseRoute, loginRoutes.postRoutes());
-    }
+  private middleware(corsConfig?: CorsOptions) {
+    this.app.use(express.json());
+    this.app.use(cors(corsConfig));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(errorMiddleware);
+  }
 }
---
+++ src/middlewares/error.middlewares.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import { ErrorsHelpers } from '../helpers/errors.helpers';
+
+export const errorMiddleware = (
+  error: Error & Partial<ErrorsHelpers>,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const statusCode = error.statusCode ?? 500;
+  const message = error.statusCode ? error.message : 'Internal Server Error';
+  return response.status(statusCode).json({ message });
+};

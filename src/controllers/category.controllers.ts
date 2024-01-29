8d049c6314d3b4cdd5a848d56d52cc614fcae8de
Cezar Fernando <czarfbc@gmail.com>
2024-01-29 17:52:01 -0300

    category middleware errors

--- src/controllers/category.controllers.ts
+++ src/controllers/category.controllers.ts
@@ -1,74 +1,75 @@
-import {CategoryServices} from "../services/category.services";
-import {NextFunction, Request, Response} from 'express';
+import { CategoryServices } from '../services/category.services';
+import { NextFunction, Request, Response } from 'express';
 
 class CategoryControllers {
-    private categoryServices: CategoryServices;
+  private categoryServices: CategoryServices;
 
-    constructor() {
-        this.categoryServices = new CategoryServices();
-    }
+  constructor() {
+    this.categoryServices = new CategoryServices();
+  }
 
-    async createCategory(request: Request, response: Response, next: NextFunction) {
-        const {name, description} = request.body;
-        try {
-            const result = await this.categoryServices.createCategory({
-                name,
-                description,
-            });
+  async createCategory(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    const { name, description } = request.body;
+    const result = await this.categoryServices.createCategory({
+      name,
+      description,
+    });
 
-            return response.status(201).json(result);
-        } catch (error: any) {
-            next(error);
-            return response.status(401).json(error.message);
-        }
-    }
+    return response.status(201).json(result);
+  }
 
-    async listAllCategories(request: Request, response: Response, next: NextFunction) {
-        try {
-            const result = await this.categoryServices.getAllCategories();
-            return response.status(200).json(result);
-        } catch (error: any) {
-            next(error);
-            return response.status(401).json(error.message);
-        }
-    }
-       async updateCategory(request: Request, response: Response, next: NextFunction) {
-        const {id} = request.params;
-        const {name, description} = request.body;
-        
-        try {
-            const idnumber: number = parseInt(id);
-            const category = await this.categoryServices.updateCategory({
-                id: idnumber,
-                name,
-                description,
-            });
+  async listAllCategories(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    const result = await this.categoryServices.getAllCategories();
 
-            return response.status(201).json(category);
-        } catch (error: any) {
-            next(error);
-            return response.status(401).json(error.message);
-        }
-    }
-     async deleteCategoryById(request: Request, response: Response, next: NextFunction) {
-        try {
-            const {id} = request.params;
-            const result = await this.categoryServices.deleteById(id);
-            return response.status(200).json(result);
-        } catch (error: any) {
-            next(error);
-            return response.status(401).json(error.message);
-        }
-    }
-    async deleteAllCategories(request: Request, response: Response, next: NextFunction) {
-        try {
-            const result = await this.categoryServices.deleteAllCategories();
-            return response.status(204).json(result);
-        } catch (error: any) {
-            next(error);
-            return response.status(401).json(error.message);
-        }
-    }
+    return response.status(200).json(result);
+  }
+
+  async updateCategory(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    const { id } = request.params;
+    const { name, description } = request.body;
+
+    const idnumber: number = parseInt(id);
+    const category = await this.categoryServices.updateCategory({
+      id: idnumber,
+      name,
+      description,
+    });
+
+    return response.status(201).json(category);
+  }
+
+  async deleteCategoryById(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    const { id } = request.params;
+    const result = await this.categoryServices.deleteById(id);
+
+    return response.status(200).json(result);
+  }
+
+  async deleteAllCategories(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    const result = await this.categoryServices.deleteAllCategories();
+
+    return response.status(204).json(result);
+  }
 }
 
-export {CategoryControllers};
+export { CategoryControllers };

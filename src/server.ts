1ae152ccdad57c4e5f7a8edfe1e1eb7ed32e77f0
carlitossaul <carlitossaul.h@gmail.com>
2024-01-31 18:55:55 -0300

    fix cors set on async mode

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
 import { App } from './app';
 
-const corsConfig = {
-  origin: ['https://eat-eating-web-app.vercel.app', 'http://localhost:3000'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  optionsSuccessStatus: 204,
+export type CorsMiddleware = (req: Request, res: Response, next: NextFunction) => void;
+
+const corsConfig : CorsMiddleware = (req, res, next) => {
+  const allowedOrigins = ['https://eat-eating-web-app.vercel.app', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin || '')) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, HEAD, PATCH");
+  next();
 };
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3003;
@@ -11,3 +21,9 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3003;
 const app = new App(corsConfig);
 
 app.listen(PORT);
+
+  // const corsConfig = {
+  //   origin: ['https://eat-eating-web-app.vercel.app', 'http://localhost:3000'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   optionsSuccessStatus: 204,
+  // };

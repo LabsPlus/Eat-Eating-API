644e19e3fa53cfb0dc92e2137253364cdede4b72
Lucivando Sousa <lucivandosousa@gmail.com>
2024-03-08 15:07:43 -0300

    feat: adicionar docs das rotas listallusers e deleteuser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -11,11 +11,65 @@ class UserRoutes {
         this.userController = new UserControllers();
     }
 
+    /**
+     * @swagger
+     * /user/list-all-users:
+     *   get:
+     *     summary: Listar os usuários do sistema
+     *     description: Listar os usuários do sistema
+     *     tags:
+     *       - Usuários
+     *     responses:
+     *       '200':
+     *         description: Usuários retornados com sucesso
+     *         content:
+     *           application/json:
+     *             examples:
+     *               exemple:
+     *                 value:
+     *                   user:
+     *                     id: 1
+     *                     dailyMeals: 1
+     *                     person:
+     *                       name: Nahueld
+     *                       cpf: ''
+     *                       born: '2024-03-08T13:35:27.896Z'
+     *                       Administrator: null
+     *                     category:
+     *                       name: FUNCIONARIO
+     *                     typeGrant:
+     *                       name: INTEGRAL
+     *                     loginUser:
+     *                       email: Nahueld@labsif.com.br
+     *                       emailRecovery: Nahueld@labsif.com.br
+     *                   enrolment: '5555552'
+     */
     getRoutes() {
         this.router.get('/list-all-users', this.userController.getAllUsers.bind(this.userController))
         return this.router;
     }
 
+    /**
+     * @swagger
+     * /user/delete-user/{id}:
+     *   delete:
+     *     summary: Excluir um usuário do sistema
+     *     description: Excluir um usuário do sistema
+     *     tags:
+     *       - Usuários
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: ID do usuário
+     *         schema:
+     *           type: integer
+     *     responses:
+     *       '200':
+     *         description: User successfully deleted
+     *       '404':
+     *         description: Usuário não encontrado
+     */
     deleteRoutes() {
         this.router.delete('/delete-user/:id', this.userController.deleteUserById.bind(this.userController))
         return this.router;

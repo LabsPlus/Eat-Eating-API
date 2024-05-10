5c672b2d59286865e65cf6cf67b3a61f2f71f12d
bruno34154 <www.brunomesenga@gmail.com>
2024-05-10 20:19:57 -0300

    Back-86b0cntzp-Adding logic to edit tickets

--- src/database/repositories/ticket.repositories/ticket.dals.ts
+++ src/database/repositories/ticket.repositories/ticket.dals.ts
@@ -1,3 +1,5 @@
+import { IPurchaseTicket } from '../../../intefaces/ticket.interfaces';
+import { IUserTicketCountFind } from '../../../intefaces/userTicketCount.interfaces';
 import { prisma } from '../../prisma.databases';
 
 class TicketDALs {
@@ -8,6 +10,26 @@ class TicketDALs {
       data: {
         userTicketsCountId,
       },
+    });
+
+    return result;
+  }
+  async findQuantityTickets({userTicketsCountId, quantity}: IUserTicketCountFind){
+   const ticketsToDelete = await prisma.ticket.findMany({
+      where: { userTicketsCountId },
+      take: quantity
+    });
+
+    return ticketsToDelete;
+  }
+
+  async removeTickets(ticketIdsToDelete: number[]){
+     const result = await prisma.ticket.deleteMany({
+      where: {
+        id: {
+          in: ticketIdsToDelete // Remova os tickets cujos IDs estão na lista de IDs a serem excluídos
+        }
+      }
     });
 
     return result;

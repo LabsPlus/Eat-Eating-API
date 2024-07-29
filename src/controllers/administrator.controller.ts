0e3c6ef707c6fa3d8dc578476f1bdc8d908d62c3
bruno34154 <www.brunomesenga@gmail.com>
2024-07-29 15:11:31 -0300

    back-86b1aefek-update-administrator

--- src/controllers/administrator.controller.ts
+++ src/controllers/administrator.controller.ts
@@ -16,6 +16,14 @@ async createAdministrator(request: Request, response: Response, next: NextFuncti
         return response.status(200).json(result);
 } 
 
+async updateAdministrator(request: Request, response: Response, next: NextFunction){
+        const {email, password, emailRecovery, name, phone, picture} = request.body;
+        const {id} = request.params;
+        const result = await this.AdministratorServices.updateAdministrator(Number(id), {email, password, emailRecovery, phone, picture, name});
+
+        return response.status(200).json(result);
+} 
+
 
 
 }
--- src/intefaces/administrator.interfaces.ts
+++ src/intefaces/administrator.interfaces.ts
@@ -6,3 +6,13 @@ export interface IAdministratorData {
   phone: string;
   picture: string;
 }
+export interface IAdministratorUpdate {
+  adminId:number;
+  personId: number;
+  email: string;
+  password: string;
+  emailRecovery: string;
+  name: string;
+  phone: string;
+  picture: string;
+}
